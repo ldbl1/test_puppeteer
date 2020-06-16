19a4d19fdeea9e05113f7d6d996fe4b8b7a26f86
Lorodo <barrioleal1994@gmail.com>
2020-06-16 14:27:59 +0200

    prueba 1

--- hn.js
+++ hn.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'test_puppeter/erexample.png'});
+  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
+  await page.pdf({path: 'test_puppeteer/hn.pdf', format: 'A4'});
 
   await browser.close();
 })();
